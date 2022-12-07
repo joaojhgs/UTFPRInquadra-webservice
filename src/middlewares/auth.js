6f68829bf402a0cdc3f079d3d7a4e92f479851f2
joaohgs <joaosperandio@alunos.utfpr.edu.br>
2022-12-07 08:05:20 -0300

    #20 - Fix: login query error handling

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,11 +8,12 @@ module.exports = (req, res, next, role) => {
     const token = authorization?.split(' ')[1]
     console.log(token);
     try {
-        const result = jsonwebtoken.verify(token, process.env.JWT_SECRET_TOKEN, {complete: true});
-        if(result.payload.role !== role && result.payload.role !== 'ADMIN') return res.send("Permissão insuficiente")
+        const result = jsonwebtoken.verify(token, process.env.JWT_SECRET_TOKEN, { complete: true });
+        if (result.payload.role !== role && result.payload.role !== 'ADMIN') return res.send("Permissão insuficiente")
     }
     catch {
-        return res.send("Token de autenticação invalido")
+        res.status(500)
+        res.render('error', { error: "Token de autenticação invalido" });
     }
     return next()
 }
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -4,30 +4,33 @@ const { PrismaClient } = require('../../prisma/@generated');
 const prisma = new PrismaClient()
 module.exports = app => {
 
-    app.post('/login', async (req, res) => {
-        const {ra, password} = req.body;
-        if(!ra) return res.send("RA invalido");
-        if(!password) return res.send("Senha invalida");
+    app.post('/login', async (req, res, next) => {
+        console.log(req.body)
+        const { ra, password } = req.body;
+        if (!ra) {
+            return res.status(500).send('RA invalido!')
+        };
+        if (!password) return res.status(500).send('Senha invalida!');
 
         const user = await prisma.user.findFirst({
-            where:{
+            where: {
                 ra: ra,
             },
-            select:{
+            select: {
                 id: true,
                 role: true,
                 password: true,
             }
         })
 
-        if(!user) return res.send("Usuario não encontrado");
-        bcrypt.compare(password, user.password, function(err, result) {
+        if (!user) return res.send("Usuario não encontrado");
+        bcrypt.compare(password, user.password, function (err, result) {
             if (result) {
-                return res.send(jsonwebtoken.sign({id: user.id, ra: ra, role: user.role}, process.env.JWT_SECRET_TOKEN, {expiresIn: process.env.JWT_EXPIRATION_TIME}));
-            }   
-            else {
-                return res.send("Senha invalida");
+                return res.send(jsonwebtoken.sign({ id: user.id, ra: ra, role: user.role }, process.env.JWT_SECRET_TOKEN, { expiresIn: process.env.JWT_EXPIRATION_TIME }));
             }
-          });
+            else {
+                if (!password) return res.status(500).send('Senha invalida!');
+            }
+        });
     })
 }
